8bdb1bafa6580fec71d3f4ebbbc2cdcaa8093d3c
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-07 20:11:39 +0100

    chore: created reuseable component

---
+++ src/common/utils.tsx
@@ -0,0 +1,18 @@
+import {
+    widthPercentageToDP as wdp,
+    heightPercentageToDP as hdp,
+  } from 'react-native-responsive-screen';
+  
+  const CustomHeight = 812;
+  const CustomWidth = 375;
+  
+  export const hp = (value: number) => {
+    const dimension = (value / CustomHeight) * 100;
+    return hdp(`${dimension}%`);
+  };
+  
+  export const wp = (value: number) => {
+    const dimension = (value / CustomWidth) * 100;
+    return wdp(`${dimension}%`);
+  };
+  
