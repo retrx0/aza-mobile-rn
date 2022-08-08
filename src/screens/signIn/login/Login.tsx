c8a245388752f805693de48fbeb4aba1a1b60562
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-08 15:57:34 +0100

    refactor: sigin screens

---
+++ src/screens/signIn/login/Login.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Button } from "../../../common/Button";
+import { CountryBox } from "../../../components/CountryInput";
+import { Header } from "../../../common/header";
+import { LoginStyles as styles } from "./styles";
+import { SocialSignInList } from "./LoginCard";
+import { SocialSignInCard } from "../../../components/SignInButtons";
+
+type SignInProp = {
+  onCountryPress: () => void;
+  onChangeText: (text: string) => void;
+  onSendOtp: () => void;
+  phoneNumber: string;
+  short_name: string;
+};
+
+const Login = (props: SignInProp) => {
+  const { onCountryPress, onChangeText, onSendOtp, phoneNumber } = props;
+  return (
+    <>
+      <Header
+        heading='phone number'
+        headerStyle={styles.header}
+        descriptionStyle={undefined}
+        description={""}
+      />
+      <CountryBox
+        onPress={onCountryPress}
+        value={phoneNumber}
+        onChangeText={onChangeText}
+        onSubmitEditing={onSendOtp}
+        code={""}
+        short_name={""}
+      />
+      <Button
+        title='Continue'
+        style={styles.button}
+        styleText={styles.sendOTPButton}
+        onPressButton={onSendOtp}
+      />
+      <Text style={styles.orText}>OR</Text>
+      <View style={styles.signupOptions}>
+        {SocialSignInList.map((item, index) => {
+          return (
+            <SocialSignInCard
+              key={index}
+              icon={item.icon}
+              connect={item.connect}
+              onPress={() => {}}
+            />
+          );
+        })}
+      </View>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/screens/signIn/login/LoginCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AppleIcon, FacebookIcon, GoogleIcon } from "../../../../assets/svg";
+
+export const SocialSignInList = [
+  {
+    connect: "Connect Apple Account",
+    icon: <AppleIcon />,
+  },
+  {
+    connect: "Connect with Facebook",
+    icon: <FacebookIcon />,
+  },
+  {
+    connect: "Connect Google Account",
+    icon: <GoogleIcon />,
+  },
+];
