3766ce9b535a542c09eb9e3ca77a4ef548daa75f
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-11 20:59:22 +0100

    chore: install and refactor app packages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
-import Otp from "./src/screens/otp/otp";
-import Signin from "./src/screens/signIn/signIn";
-import Welcome from "./src/screens/welcomeBck/welcome";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -18,7 +14,7 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <StatusBar />
-        <Signin />
+        {/* <Signin /> */}
         {/* <Otp /> */}
         {/* <Welcome /> */}
         <Navigation colorScheme={colorScheme} />
