0280c51f3550d4813b30e1a8be6d5c09ab334c4f
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-11 21:06:26 +0100

    refactor: signup screen

--- src/screens/auth/signup/SignUpProfileSetupScreen.tsx
+++ src/screens/auth/signup/SignUpProfileSetupScreen.tsx
@@ -1,6 +1,6 @@
-import { Picker } from "@react-native-picker/picker";
+// import { Picker } from "@react-native-picker/picker";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useState } from "react";
 import CommonStyles from "../../../common/styles/CommonStyles";
 import SpacerWrapper from "../../../common/util/SpacerWrapper";
 import BackButton from "../../../components/buttons/BackButton";
@@ -8,8 +8,22 @@ import ButtonLg from "../../../components/buttons/ButtonLg";
 import BoxTextInput from "../../../components/input/BoxTextInput";
 import { Text, View } from "../../../components/Themed";
 import { SignUpStackProps } from "./SignUpNavigator";
+import RNPickerSelect from "react-native-picker-select";
+import { GENDER } from "../signin/SignInCard";
+import { Primary } from "../../../common/colors";
+// import { wp } from "../../../common/utils";
+// import { hp } from "../../../common/util/utils";
+import { Picker } from "@react-native-picker/picker";
 
-const SignUpEmailScreen = ({ navigation }: NativeStackScreenProps<SignUpStackProps>) => {
+const SignUpEmailScreen = ({
+  navigation,
+}: NativeStackScreenProps<SignUpStackProps>) => {
+  // const [gender, setGender] = useState(GENDER);
+  // const placeholder = {
+  //   label: "Select gender..",
+  //   value: null,
+  //   color: Primary,
+  // };
   return (
     <SpacerWrapper>
       <BackButton onPress={() => navigation.goBack()} />
@@ -17,11 +31,47 @@ const SignUpEmailScreen = ({ navigation }: NativeStackScreenProps<SignUpStackPro
         <Text style={[CommonStyles.headerText]}>Profile Setup</Text>
         <Text style={[CommonStyles.bodyText]}>Set up your account</Text>
       </View>
-      <BoxTextInput placeHolder="Full Name" required value="John Appleased" onChange={() => {}} />
-      <BoxTextInput placeHolder="Email" required value="johnappleased@apple.com" onChange={() => {}} />
+      <BoxTextInput
+        placeHolder='Full Name'
+        required
+        value='John Appleased'
+        onChange={() => {}}
+      />
+      <BoxTextInput
+        placeHolder='Email'
+        required
+        value='johnappleased@apple.com'
+        onChange={() => {}}
+      />
+      {/* <RNPickerSelect
+        placeholder={placeholder}
+        onValueChange={(value) => {
+          setGender(value);
+          console.log(value);
+        }}
+        value={gender}
+        items={GENDER}
+        style={{
+          viewContainer: {
+            paddingHorizontal: wp(10),
+            width: "90%",
+            height: hp(55),
+            borderWidth: 0.7,
+            borderColor: Primary,
+            borderRadius: 10,
+            marginTop: hp(7),
+            marginLeft: hp(20),
+            paddingVertical: hp(20),
+          },
+          placeholder: {
+            fontSize: 20,
+            paddingVertical: hp(10),
+          },
+        }}
+      /> */}
       <Picker collapsable>
-        <Picker.Item label="male" value={"Male"} />
-        <Picker.Item label="female" value={"Female"} />
+        <Picker.Item label='male' value={"Male"} />
+        <Picker.Item label='female' value={"Female"} />
       </Picker>
       <ButtonLg
         color={"#000"}
@@ -29,7 +79,7 @@ const SignUpEmailScreen = ({ navigation }: NativeStackScreenProps<SignUpStackPro
         onPress={() => {
           navigation.navigate("SignUpPassword");
         }}
-        title="Continue"
+        title='Continue'
       />
     </SpacerWrapper>
   );
