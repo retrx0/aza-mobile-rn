a6ac0d7c9bebf623f3e585d95c695b15c85b99fd
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-15 23:27:20 +0100

    refactor: BackButton

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { BackIcon } from "../../../assets/svg";
+import SpacerWrapper from "../../common/util/SpacerWrapper";
+import Container from "../../common/util/SpacerWrapper";
 import { hp, wp } from "../../common/util/utils";
 import Colors from "../../constants/Colors";
 import useColorScheme from "../../hooks/useColorScheme";
 import { Text, useThemeColor } from "../Themed";
 
 const BackButton = (props: { onPress: () => void }) => {
-  const color = useThemeColor({ light: Colors.light.text, dark: Colors.dark.text }, "text");
+  const color = useThemeColor(
+    { light: Colors.light.text, dark: Colors.dark.text },
+    "text"
+  );
   return (
     <TouchableOpacity style={styles.backContainer} onPress={props.onPress}>
       <BackIcon color={color} size={14} />
