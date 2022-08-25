3a368e8ae18a9ed1a1ba815cdef259b2a12e2296
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-08-25 15:33:50 +0100

    refactor: vault tab screens

--- src/screens/tabs/vault/Vault.tsx
+++ src/screens/tabs/vault/Vault.tsx
@@ -3,35 +3,42 @@ import { RootTabScreenProps } from "../../../../types";
 import Button from "../../../components/buttons/Button";
 import { Text, View } from "../../../components/Themed";
 import { Header } from "../../../components/text/header";
-import { hp } from "../../../common/util/LayoutUtil";
 import { ArrowDown } from "../../../../assets/svg";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import SpacerWrapper from "../../../common/util/SpacerWrapper";
 import CommonStyles from "../../../common/styles/CommonStyles";
-import styles from "../../onboarding/OnboardingStyles";
-import { VaultStackProps } from "./VaultNavigator";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 const Vault = ({ navigation }: RootTabScreenProps<"Vault">) => {
   return (
     <SpacerWrapper>
       <View>
         <View style={[CommonStyles.vaultContainer]}>
-          <Header heading="Vault" description={""} headerStyle={[CommonStyles.vault]} descriptionStyle={undefined} />
+          <Header
+            heading='Vault'
+            description={""}
+            headerStyle={[CommonStyles.vault]}
+            descriptionStyle={undefined}
+          />
           <Image
             source={require("../../../../assets/images/Undraw.png")}
-            resizeMode="cover"
+            resizeMode='cover'
             style={[CommonStyles.undraw]}
           />
           <Text style={[CommonStyles.vaultText]}>You dont have any vaults</Text>
           <View style={CommonStyles.createVaultContainer}>
-            <Text style={[CommonStyles.createNewVault]}>Click New Vault to create a new vault</Text>
+            <Text style={[CommonStyles.createNewVault]}>
+              Click New Vault to create a new vault
+            </Text>
             <TouchableOpacity>
               <ArrowDown />
             </TouchableOpacity>
           </View>
         </View>
-        <Button title="New Vault" onPressButton={() => navigation.navigate("Common", { screen: "NewVault" })} />
+        <Button
+          title='New Vault'
+          onPressButton={() =>
+            navigation.navigate("Common", { screen: "NewVault" })
+          }
+        />
       </View>
     </SpacerWrapper>
   );
