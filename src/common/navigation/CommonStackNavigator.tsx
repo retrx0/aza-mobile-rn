13455e564882b478c22bb429624044649e68efa0
mubeess <mubarakibrahim2015@gmail.com>
2022-09-04 07:11:04 +0100

    ready for PR

--- src/common/navigation/CommonStackNavigator.tsx
+++ src/common/navigation/CommonStackNavigator.tsx
@@ -1,4 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { TouchableOpacity } from "react-native";
+import { BackIcon } from "../../../assets/svg";
+import { Text } from "../../components/Themed";
+import useColorScheme from "../../hooks/useColorScheme";
 import { AirtimeTabs, CharityTabs } from "../../navigation/PaymensNavigation";
 import BvnVerificationNavigator from "../../screens/bvn/BvnVerificationNavigator";
 import StatusScreen from "../../screens/status/StatusScreen";
@@ -17,6 +21,7 @@ import { CommonStackParamList } from "./types";
 const Stack = createNativeStackNavigator<CommonStackParamList>();
 
 const CommonStack = () => {
+  const scheme=useColorScheme()
   return (
     <Stack.Navigator defaultScreenOptions={{ headerShown: false }}>
       <Stack.Group screenOptions={{ headerShown: false }}>
@@ -28,7 +33,32 @@ const CommonStack = () => {
 
       {/* Payments */}
 
-      <Stack.Group>
+      <Stack.Group screenOptions={(props)=>(
+      {
+        headerTitleAlign: 'center',
+        headerLeft:()=>(
+          <TouchableOpacity 
+          onPress={()=>props.navigation.goBack()}
+          style={{
+            flexDirection:'row',
+            alignItems:'center'
+          }}>
+          <BackIcon color={scheme=='light'?'#000000':'#ffffff'} size={24}/>
+          <Text style={{marginLeft:5}}>Back</Text>
+          </TouchableOpacity>
+         
+        ),
+        headerStyle:{
+        backgroundColor:'',
+        },
+        headerTransparent:true,
+        headerTitleStyle:{
+          fontSize:16,
+          fontWeight:'600'
+        }
+        
+      }
+    )}>
         <Stack.Screen options={{ title: "Airtime & Data" }} name="AirtimeData" component={AirtimeTabs} />
         <Stack.Screen options={{ presentation: "fullScreenModal", title: "" }} name="Pie" component={Pie} />
         <Stack.Screen options={{ title: "Confirmation" }} name="Confirm" component={Confirmation} />
