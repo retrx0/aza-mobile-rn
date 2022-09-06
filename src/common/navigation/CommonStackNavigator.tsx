2bfb20381d3cb886a47521e0c5662b42964e4b57
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-09-06 11:49:06 +0100

    refactor: types

--- src/common/navigation/CommonStackNavigator.tsx
+++ src/common/navigation/CommonStackNavigator.tsx
@@ -1,16 +1,68 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BvnVerificationNavigator from "../../screens/bvn/BvnVerificationNavigator";
 import AddVault from "../../screens/tabs/Vault/AddVault";
-// import ArchievedVault from "../../screens/tabs/Vault/ArchievedVault";
 import ConfirmDeleteVault from "../../screens/tabs/Vault/ConfirmDeleteVault";
-import DeleteVault from "../../screens/tabs/Vault/DeleteVault";
+import DeleteVault from "../../screens/tabs/Vault/LockVault";
 import NewVault from "../../screens/tabs/Vault/NewVault";
+import VaultActivity from "../../screens/tabs/Vault/withdrawToAza/VaultActivity";
+import VaultDetails from "../../screens/tabs/Vault/withdrawToAza/VaultDetails";
 import VaultSuccessful from "../../screens/tabs/Vault/VaultSuccessful";
-import VaultWithdraw from "../../screens/tabs/Vault/VaultwithdrawalConfirmation";
-import VaultWithdrawsuccessful from "../../screens/tabs/Vault/VaultWithdrawSuccessful";
+import VaultWithdrawsuccessful from "../../screens/tabs/Vault/withdrawToAza/VaultWithdrawSuccessful";
 import { CommonStackParamList } from "./types";
+import { Platform } from "react-native";
+import useColorScheme from "../../hooks/useColorScheme";
+import { Text, View } from "../../components/Themed";
+import CommonStyles from "../styles/CommonStyles";
+import BackButton from "../../components/buttons/BackButton";
+import SpacerWrapper from "../util/SpacerWrapper";
+import VaulToBank from "../../screens/tabs/Vault/withdrawToBank/VaultWithdrawToBank";
+import VaultWithdrawConfirmation from "../../screens/tabs/Vault/withdrawToBank/VaultWithdrawToBankConfirmation";
+import VaultToAza from "../../screens/tabs/Vault/withdrawToAza/VaultwithdrawalConfirmation";
+import VaultToBankSuccessfull from "../../screens/tabs/Vault/withdrawToBank/VaultToBankSuccessful";
+import TransactionHistory from "../../screens/tabs/Vault/TransactionHistory";
+import LockVault from "../../screens/tabs/Vault/LockVault";
+import ArchievedVault from "../../screens/tabs/Vault/ArchievedVault";
 
 const Stack = createNativeStackNavigator<CommonStackParamList>();
+const Tab = createMaterialTopTabNavigator<CommonStackParamList>();
+
+export const TopBar = ({ navigation }: { navigation: any }) => {
+  const scheme = useColorScheme();
+
+  return (
+    <SpacerWrapper>
+      <View style={[CommonStyles.topTab]}>
+        <View style={{ marginLeft: 20 }}>
+          <BackButton
+            onPress={() => {
+              navigation.getParent()?.navigate("AddVault");
+            }}
+          />
+        </View>
+        <Text style={CommonStyles.vaultTab}>Vault</Text>
+      </View>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarItemStyle: {
+            borderRadius: 100,
+            marginTop: Platform.OS == "android" ? 50 : 0,
+          },
+          tabBarIndicatorStyle: {
+            borderWidth: 1,
+            borderColor: scheme == "light" ? "#000000" : "#ffffff",
+          },
+          tabBarLabelStyle: {
+            textTransform: "capitalize",
+          },
+        }}
+        initialRouteName='details'>
+        <Tab.Screen component={VaultDetails} name='details' />
+        <Tab.Screen component={VaultActivity} name='activity' />
+      </Tab.Navigator>
+    </SpacerWrapper>
+  );
+};
 
 const CommonStack = () => {
   return (
@@ -25,8 +77,8 @@ const CommonStack = () => {
 
         <Stack.Screen
           options={{ headerShown: false }}
-          name='DeleteVault'
-          component={DeleteVault}
+          name='LockVault'
+          component={LockVault}
         />
         <Stack.Screen
           options={{ headerShown: false }}
@@ -43,21 +95,48 @@ const CommonStack = () => {
           name='ConfirmDeleteVault'
           component={ConfirmDeleteVault}
         />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='VaulToBank'
+          component={VaulToBank}
+        />
 
-        {/* <Stack.Screen
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='VaultToAza'
+          component={VaultToAza}
+        />
+        <Stack.Screen
           options={{ headerShown: false }}
           name='ArchievedVault'
           component={ArchievedVault}
-        /> */}
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name='VaultWithdraw'
-          component={VaultWithdraw}
         />
         <Stack.Screen
           options={{ headerShown: false }}
           name='VaultWithdrawsuccessful'
           component={VaultWithdrawsuccessful}
+        />
+
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='TopBar'
+          component={TopBar}
+        />
+
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='VaultWithdrawConfirmation'
+          component={VaultWithdrawConfirmation}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='VaultToBankSuccessfull'
+          component={VaultToBankSuccessfull}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name='TransactionHistory'
+          component={TransactionHistory}
         />
       </Stack.Group>
     </Stack.Navigator>
