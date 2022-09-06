40423f59b1ea65c255c8aafa06f7fdccc3abe82e
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-09-06 11:49:26 +0100

    chore: asstes png index

---
+++ assets/images/index.ts
@@ -0,0 +1,5 @@
+export const Aleemz = require("./Aleemz.png");
+export const Chess = require("./Chess.png");
+export const AccessBank = require("./AccessBank.png");
+export const UbaBank = require("./UbaBank.png");
+export const Swift = require("./Swift.png");
