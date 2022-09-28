e5a2c760438fbede64cc1009e8f72c09ce53a117
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-09-28 14:48:59 +0100

    refactor

--- src/screens/tabs/settings/screens/LoginOptionsScreen.tsx
+++ src/screens/tabs/settings/screens/LoginOptionsScreen.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { CommonScreenProps } from '../../../../common/navigation/types'
-import BackButton from '../../../../components/buttons/BackButton'
-import { Text, View } from '../../../../components/Themed'
-import Colors from '../../../../constants/Colors'
-import { hp } from '../../../../common/util/LayoutUtil'
-import ButtonLg from '../../../../components/buttons/ButtonLg'
-import SpacerWrapper from '../../../../common/util/SpacerWrapper'
+import { StyleSheet } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { CommonScreenProps } from "../../../../common/navigation/types";
+import BackButton from "../../../../components/buttons/BackButton";
+import { Text, View } from "../../../../components/Themed";
+import Colors from "../../../../constants/Colors";
+import { hp } from "../../../../common/util/LayoutUtil";
+import ButtonLg from "../../../../components/buttons/ButtonLg";
+import SpacerWrapper from "../../../../common/util/SpacerWrapper";
+import { AppleIcon, FacebookIcon, GoogleIcon } from "../../../../../assets/svg";
 
 const LoginOptionsScreen = ({
   navigation,
-}: CommonScreenProps<'LoginOptions'>) => {
+}: CommonScreenProps<"LoginOptions">) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
@@ -18,21 +19,20 @@ const LoginOptionsScreen = ({
           lightColor={Colors.light.text}
           darkColor={Colors.dark.mainText}
           style={{
-            fontFamily: 'Euclid-Circular-A-Semi-Bold',
+            fontFamily: "Euclid-Circular-A-Semi-Bold",
             fontSize: 16,
-          }}
-        >
+          }}>
           Login Options
         </Text>
       ),
       // hide default back button which only shows in android
       headerBackVisible: false,
       //center it in android
-      headerTitleAlign: 'center',
+      headerTitleAlign: "center",
       headerShadowVisible: false,
       headerLeft: () => <BackButton onPress={() => navigation.goBack()} />,
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <SpacerWrapper>
@@ -41,47 +41,46 @@ const LoginOptionsScreen = ({
           <Text
             lightColor={Colors.light.text}
             darkColor={Colors.dark.mainText}
-            style={{ fontSize: 14, fontFamily: 'Euclid-Circular-A-Medium' }}
-          >
+            style={{ fontSize: 14, fontFamily: "Euclid-Circular-A-Medium" }}>
             Login quickly by connecting your Aza account to your social media
             account.
           </Text>
         </View>
         <View>
           <ButtonLg
-            iconName="apple"
+            icon={<AppleIcon />}
             title="Connect with Apple"
             color={Colors.general.apple}
-            onPress={() => console.log('connecting with apple...')}
+            onPress={() => console.log("connecting with apple...")}
             alt={false}
           />
           <ButtonLg
-            iconName={'facebook'}
+            icon={<FacebookIcon />}
             title="Connect with Facebook"
             color={Colors.general.facebook}
-            onPress={() => console.log('connecting with facebook...')}
+            onPress={() => console.log("connecting with facebook...")}
             alt={false}
           />
           <ButtonLg
-            iconName={'google'}
+            icon={<GoogleIcon />}
             title="Connect with Google"
             color={Colors.general.google}
-            onPress={() => console.log('connecting with google...')}
+            onPress={() => console.log("connecting with google...")}
             alt={false}
           />
         </View>
       </View>
     </SpacerWrapper>
-  )
-}
+  );
+};
 
-export default LoginOptionsScreen
+export default LoginOptionsScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     paddingVertical: hp(20),
     paddingHorizontal: 15,
   },
-})
+});
