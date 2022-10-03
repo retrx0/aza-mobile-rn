b65d369985ed5e744a67e4a01b5c7f954a296432
retrx0 <61639630+retrx0@users.noreply.github.com>
2022-10-03 23:19:18 +0200

    babel fix

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_BASE_URL } from "@env";
+console.log(API_BASE_URL);
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  responseType: "json",
+  // headers: {
+  //   "X-API-KEY": "Bearer <KEY>",
+  //   Accept: "application/json",
+  // },
+});
+export default api;
