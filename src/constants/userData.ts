77e6d98834128fc48bbf6a964b39e5fabd4e5460
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-10-04 07:55:25 +0100

    refactor

---
+++ src/constants/userData.ts
@@ -0,0 +1,7 @@
+export const UserData = {
+  userAzaAccountNumber: "1234567890",
+  userAzaBalance: " 2,239,290.00",
+  userphoneNumber: "+9091234567",
+  userFullName: " Chiazondu Joseph",
+  userEmail: " chiazo@samplemail.com",
+};
