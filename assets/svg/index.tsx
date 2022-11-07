88c2bf52c69dc00ae5724a350b395d2d81dc1e26
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-11-07 14:12:53 +0100

    svg index

--- assets/svg/index.tsx
+++ assets/svg/index.tsx
@@ -773,6 +773,94 @@ export const CurrencyIcon = ({ color }: SvgIconProps) => {
   );
 };
 
+export const OpenIcon = () => {
+  return (
+    <Svg width="9" height="5" viewBox="0 0 9 5" fill="none">
+      <Path
+        d="M1.63125 0.369141L4.5 3.23164L7.36875 0.369141L8.25 1.25039L4.5 5.00039L0.75 1.25039L1.63125 0.369141Z"
+        fill="black"
+      />
+    </Svg>
+  );
+};
+
+export const AddIcon = () => {
+  return (
+    <Svg width="40" height="40" viewBox="0 0 40 40" fill="none">
+      <Path
+        d="M20.0007 36.6663C29.1673 36.6663 36.6673 29.1663 36.6673 19.9997C36.6673 10.833 29.1673 3.33301 20.0007 3.33301C10.834 3.33301 3.33398 10.833 3.33398 19.9997C3.33398 29.1663 10.834 36.6663 20.0007 36.6663Z"
+        stroke="#2AD168"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M13.334 20H26.6673"
+        stroke="#2AD168"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M20 26.6663V13.333"
+        stroke="#2AD168"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
+export const Icons = () => {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M8 19H16C18 19 19 18 19 16V8C19 6 18 5 16 5H8C6 5 5 6 5 8V16C5 18 6 19 8 19Z"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M5 9.5H7.48C8.86 9.5 9.98 10.62 9.98 12C9.98 13.38 8.86 14.5 7.48 14.5H5"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M19 9.99023H16"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M19 14H16"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M7.19922 12H7.29922"
+        stroke="white"
+        stroke-width="2"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
+
 export const WithDrawIcon = () => {
   return (
     <Svg width="16" height="16" viewBox="0 0 16 16" fill="none">
@@ -2310,3 +2398,14 @@ export const UndrawCreditCardIcon = ({ color }: SvgIconProps) => {
     </Svg>
   );
 };
+
+// export const ExitIcon = () => {
+//   return (
+//     <Svg width="30" height="30" viewBox="0 0 30 30" fill="none">
+//       <Path
+//         d="M23.4 8.29201L21.708 6.60001L15 13.308L8.29201 6.60001L6.60001 8.29201L13.308 15L6.60001 21.708L8.29201 23.4L15 16.692L21.708 23.4L23.4 21.708L16.692 15L23.4 8.29201Z"
+//         fill="#070648"
+//       />
+//     </Svg>
+//   );
+// };
