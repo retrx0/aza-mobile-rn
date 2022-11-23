33330158eae1bff2df5ab3f2561c0955976de38c
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-11-23 16:58:56 +0100

    added icons

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,6 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 export const Ntel = require("./common/ntel.png");
 export const Ie = require("./common/ie.png");
+export const AEDC = require("./common/AEDC.png");
+export const PH = require("./common/PH.png");
+export const Gotv = require("./common/Gotv.png");
+export const Fctwb = require("./common/Fctwb.png");
+export const Lswc = require("./common/Lswc.png");
+export const Crswb = require("./common/Crswb.png");
+export const Vws = require("./common/Vws.png");
+export const Enswc = require("./common/Enswc.png");
+
+export const Startimes = require("./common/Startimes.png");
+export const Dstv = require("./common/Dstv.png");
+
+export const EEDC = require("./common/EEDC.png");
+export const EKEDC = require("./common/EKEDC.png");
+export const ipnx = require("./common/ipnx.png");
+export const legend = require("./common/legend.png");
+export const smile = require("./common/smile.png");
+export const cobra = require("./common/cobra.png");
+
 export const Auth = require("./common/auth.png");
 export const Spectranet = require("./common/spectranet.png");
 export const Undraw = require("./common/Undraw.png");
@@ -30,3 +49,16 @@ export const NigerianFlag = require("./icons/NigerianFlag.png");
 
 // Vault
 export const VaultLogo = require("./icons/VaultLogo.png");
+
+export const YARA = require("./common/YARA.png");
+export const IREDE = require("./common/IREDE.png");
+export const SAVE = require("./common/SAVE.png");
+export const TIMEOUT = require("./common/TIMEOUT.png");
+export const DORCAS = require("./common/DORCAS.png");
+export const HOPE = require("./common/HOPE.png");
+export const SAINTS = require("./common/SAINTS.png");
+export const FOUNTAIN = require("./common/FOUNTAIN.png");
+export const ICICE = require("./common/ICICE.png");
+export const IET = require("./common/IET.png");
+export const REAL = require("./common/REAL.png");
+export const OVIE = require("./common/OVIE.png");
