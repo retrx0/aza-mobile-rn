a0bd2dc75bf621e3737f22ead6363b5055b06a63
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-12-06 01:38:36 +0100

    added icons

--- src/screens/tabs/payments/Pie.tsx
+++ src/screens/tabs/payments/Pie.tsx
@@ -1,71 +1,69 @@
 import { StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-import { SafeAreaView, Text, View } from "../../../components/Themed";
+import { Text, View } from "../../../components/Themed";
 import CommonStyles from "../../../common/styles/CommonStyles";
 import { PieChart } from "react-native-gifted-charts";
 import RegularText from "../../../components/text/RegularText";
-import { AIrtimeStyles } from "./airtime-screens/styles";
-import {
-  ArrowFowardIcon,
-  ArrowLeftIcon,
-  ArrowRightIcon,
-  BackIcon,
-  LoveIcon,
-} from "../../../../assets/svg";
+import { ArrowLeftIcon, ArrowRightIcon } from "../../../../assets/svg";
 import useColorScheme from "../../../hooks/useColorScheme";
 import { hp } from "../../../common/util/LayoutUtil";
+import SpacerWrapper from "../../../common/util/SpacerWrapper";
 
 const data = [
   { value: 54, color: "#2A9E17", text: "Cable Tv" },
   { value: 40, color: "#ED8A0A", text: "Internet" },
   { value: 20, color: "#753FF6", text: "Charity" },
 ];
+
 export default function Pie() {
-  const scheme = useColorScheme();
   const colorScheme = useColorScheme();
+  // const [date, setDate] = useState("10-06-2022");
 
   return (
-    <SafeAreaView style={[CommonStyles.parentContainer]}>
-      <View style={styles.container}>
+    <SpacerWrapper>
+      <View style={[CommonStyles.vaultcontainer]}>
         <View style={styles.month}>
           <TouchableOpacity>
             <ArrowLeftIcon
-              color={scheme == "light" ? "#292D32" : "white"}
+              color={colorScheme == "light" ? "#292D32" : "white"}
               size={0}
             />
           </TouchableOpacity>
-
           <Text style={styles.monthText}>Jun 2022</Text>
+
           <TouchableOpacity>
             <ArrowRightIcon
-              color={scheme == "light" ? "#292D32" : "white"}
+              color={colorScheme == "light" ? "#292D32" : "white"}
               size={16}
             />
           </TouchableOpacity>
         </View>
-        <PieChart
-          textBackgroundRadius={26}
-          textColor="#ffffff"
-          donut={true}
-          data={data}
-          focusOnPress
-          showValuesAsLabels
-          centerLabelComponent={() => (
-            <View style={[styles.centerLabel]}>
-              <Text darkColor="#000000" lightColor="#000000">
-                Total
-              </Text>
-              <RegularText
-                text={"\u20A638,000"}
-                style={[
-                  {
-                    color: colorScheme === "dark" ? "#000000" : "#000000",
-                  },
-                ]}
-              />
-            </View>
-          )}
-        />
+        <View style={{ alignSelf: "center" }}>
+          <PieChart
+            textBackgroundRadius={26}
+            textColor="#ffffff"
+            donut={true}
+            data={data}
+            focusOnPress
+            showValuesAsLabels
+            centerLabelComponent={() => (
+              <View style={[styles.centerLabel]}>
+                <Text darkColor="#000000" lightColor="#000000">
+                  Total
+                </Text>
+                <RegularText
+                  text={"\u20A638,000"}
+                  style={[
+                    {
+                      color: colorScheme === "dark" ? "#000000" : "#000000",
+                    },
+                  ]}
+                />
+              </View>
+            )}
+          />
+        </View>
+
         <View style={styles.labels}>
           {data.map((item, ind) => (
             <View key={ind.toString()} style={styles.individualLabel}>
@@ -76,7 +74,7 @@ export default function Pie() {
           ))}
         </View>
       </View>
-    </SafeAreaView>
+    </SpacerWrapper>
   );
 }
 
@@ -96,6 +94,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     width: "80%",
     marginTop: 60,
+    alignSelf: "center",
   },
   individualLabel: {
     flexDirection: "row",
@@ -111,13 +110,30 @@ const styles = StyleSheet.create({
   month: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-around",
-    width: "50%",
     marginBottom: 30,
+    alignSelf: "center",
   },
   monthText: {
     fontWeight: "600",
     fontFamily: "Euclid-Circular-A-Semi-Bold",
     fontSize: hp(16),
+    marginRight: hp(10),
+    marginLeft: hp(10),
+  },
+  datecontainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 20,
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
   },
 });
