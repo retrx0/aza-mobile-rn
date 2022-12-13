fe64da29b93e79257886975c652c16550ba75ce7
Habeeb10 <79145763+Habeeb10@users.noreply.github.com>
2022-12-13 11:41:22 +0100

    vault

--- src/screens/tabs/vault/components/VaultModal.tsx
+++ src/screens/tabs/vault/components/VaultModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, Image, useColorScheme } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 import Modal from "react-native-modal";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import CommonStyles from "../../../../common/styles/CommonStyles";
@@ -12,6 +12,7 @@ import { wp } from "../../../../common/util/LayoutUtil";
 import { NAIRA_UNICODE } from "../../../../constants/AppConstants";
 import Divider from "../../payments/sub-components/Divider";
 // import navigation from "../../../navigation";
+import useColorScheme from "../../../../hooks/useColorScheme";
 
 type DescriptionModalProps = {
   visible: boolean;
@@ -24,136 +25,149 @@ const VaultModal = ({
   navigation,
   setModalVisible,
 }: DescriptionModalProps) => {
-  const colorScheme = useColorScheme();
   // const navigation = useNavigation();
+  const colorScheme = useColorScheme();
 
   return (
-    <Modal
-      onBackdropPress={() => setModalVisible(false)}
-      isVisible={visible}
-      style={{ justifyContent: "flex-end", margin: 0 }}>
-      <TouchableOpacity
-        style={{
-          backgroundColor: "transparent",
-          alignItems: "flex-end",
-          marginBottom: 10,
-          marginRight: 10,
-        }}
-        onPress={() => setModalVisible(false)}>
-        <CloseCircleLargeIcon />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-          paddingHorizontal: 15,
-          backgroundColor: colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
-        }}>
+    <View>
+      <Modal
+        onBackdropPress={() => setModalVisible(false)}
+        isVisible={visible}
+        style={{ justifyContent: "flex-end", margin: 0 }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "transparent",
+            alignItems: "flex-end",
+            marginBottom: 10,
+            marginRight: 10,
+          }}
+          onPress={() => setModalVisible(false)}>
+          <CloseCircleLargeIcon
+            color={colorScheme === "dark" ? "#E7E9EA" : "#E7E9EA"}
+          />
+        </TouchableOpacity>
         <View
           style={{
-            height: hp(335),
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingHorizontal: 15,
             backgroundColor: colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
           }}>
-          <Text
-            style={{
-              fontFamily: "Euclid-Circular-A-Medium",
-              fontSize: hp(24),
-              textAlign: "center",
-              fontWeight: "500",
-              marginTop: hp(20),
-              marginBottom: hp(20),
-            }}>
-            Accounts
-          </Text>
-          <Divider />
           <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginBottom: hp(30),
-              marginTop: hp(30),
+              height: hp(335),
               backgroundColor: colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
             }}>
-            <View
+            <Text
+              style={{
+                fontFamily: "Euclid-Circular-A-Medium",
+                fontSize: hp(24),
+                textAlign: "center",
+                fontWeight: "500",
+                marginTop: hp(20),
+                marginBottom: hp(20),
+              }}>
+              Accounts
+            </Text>
+            <Divider />
+            <TouchableOpacity
+              onPress={() => {
+                navigation.getParent()?.navigate("Home");
+                setModalVisible(false);
+              }}
               style={{
                 flexDirection: "row",
                 alignItems: "center",
+                justifyContent: "space-between",
+                marginBottom: hp(30),
+                marginTop: hp(30),
                 backgroundColor: colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
               }}>
-              <Image
-                source={NigeriaFlag}
-                style={{ width: wp(40), height: hp(40) }}
-              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  backgroundColor:
+                    colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
+                }}>
+                <Image
+                  source={NigeriaFlag}
+                  style={{ width: wp(40), height: hp(40) }}
+                />
+                <Text
+                  style={{
+                    fontFamily: "Euclid-Circular-A-Medium",
+                    fontSize: hp(16),
+                    textAlign: "center",
+                    fontWeight: "500",
+                    marginLeft: hp(10),
+                  }}>
+                  NGN - Naira
+                </Text>
+              </View>
+
               <Text
                 style={{
                   fontFamily: "Euclid-Circular-A-Medium",
                   fontSize: hp(16),
                   textAlign: "center",
                   fontWeight: "500",
-                  marginLeft: hp(10),
                 }}>
-                NGN - Naira
+                {`${NAIRA_UNICODE} 239,290`}
               </Text>
-            </View>
-            <Text
+            </TouchableOpacity>
+            <Divider />
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Common", {
+                  screen: "NewUserVault",
+                });
+                setModalVisible(false);
+              }}
               style={{
-                fontFamily: "Euclid-Circular-A-Medium",
-                fontSize: hp(16),
-                textAlign: "center",
-                fontWeight: "500",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: hp(30),
+                marginBottom: hp(30),
               }}>
-              {`${NAIRA_UNICODE} 239,290`}
-            </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  backgroundColor:
+                    colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
+                }}>
+                <Image
+                  style={{ width: wp(40), height: hp(40) }}
+                  source={VaultLogo}
+                />
+                <Text
+                  style={{
+                    fontFamily: "Euclid-Circular-A-Medium",
+                    fontSize: hp(16),
+                    textAlign: "center",
+                    fontWeight: "500",
+                    marginLeft: hp(10),
+                  }}>
+                  Vault
+                </Text>
+              </View>
+              <Text
+                style={{
+                  fontFamily: "Euclid-Circular-A-Medium",
+                  fontSize: hp(16),
+                  textAlign: "center",
+                  fontWeight: "500",
+                }}>
+                {`${NAIRA_UNICODE} 239,290`}
+              </Text>
+            </TouchableOpacity>
+            <Divider />
           </View>
-          <Divider />
-          <TouchableOpacity
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginTop: hp(30),
-              marginBottom: hp(30),
-            }}
-            onPress={() => {
-              navigation.getParent()?.navigate("Home");
-              setModalVisible(false);
-            }}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                backgroundColor: colorScheme === "dark" ? "#3A3D42" : "#FFFFFF",
-              }}>
-              <Image
-                style={{ width: wp(40), height: hp(40) }}
-                source={VaultLogo}
-              />
-              <Text
-                style={{
-                  fontFamily: "Euclid-Circular-A-Medium",
-                  fontSize: hp(16),
-                  textAlign: "center",
-                  fontWeight: "500",
-                  marginLeft: hp(10),
-                }}>
-                Home
-              </Text>
-            </View>
-            <Text
-              style={{
-                fontFamily: "Euclid-Circular-A-Medium",
-                fontSize: hp(16),
-                textAlign: "center",
-                fontWeight: "500",
-              }}>
-              {`${NAIRA_UNICODE} 239,290`}
-            </Text>
-          </TouchableOpacity>
-          <Divider />
         </View>
-      </TouchableOpacity>
-    </Modal>
+      </Modal>
+    </View>
   );
 };
 
