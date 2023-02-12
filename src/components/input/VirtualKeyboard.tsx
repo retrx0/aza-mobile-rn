bacf7d1c2d95b46fc7a0cf1a190dab0b3427e9f9
retrx0 <61639630+retrx0@users.noreply.github.com>
2023-02-13 00:37:43 +0100

    preference loading fix

--- src/components/input/VirtualKeyboard.tsx
+++ src/components/input/VirtualKeyboard.tsx
@@ -6,22 +6,39 @@ import { useAppSelector } from "../../redux";
 import { selectAppTheme } from "../../redux/slice/themeSlice";
 import { getAppTheme } from "../../theme";
 import { View, Text } from "../../theme/Themed";
+import * as Haptics from "expo-haptics";
 
 interface IProps {
   value: string;
   setValue: (value: string) => void;
   maxLength: number;
+  withComma?: boolean;
+  allowZeroAsFirstCharacter?: boolean;
 }
 
-const VirtualKeyboard = ({ value, setValue, maxLength }: IProps) => {
+const VirtualKeyboard = ({
+  value,
+  setValue,
+  maxLength,
+  withComma,
+  allowZeroAsFirstCharacter,
+}: IProps) => {
   const appTheme = getAppTheme(useAppSelector(selectAppTheme));
 
   const onKeyPress = (key: string) => {
+    Haptics.selectionAsync();
+    let currentText = value;
+    if (key === "0" && allowZeroAsFirstCharacter) {
+      currentText += key;
+      setValue(currentText);
+      return;
+    }
+
     if ((value === "" && key === "0") || (value === "" && key === ",")) {
       //prevents '0' and ',' as first characters
       return;
     }
-    let currentText = value;
+
     if (key !== "backIcon") {
       if (currentText.length <= maxLength) {
         currentText += key;
@@ -41,6 +58,7 @@ const VirtualKeyboard = ({ value, setValue, maxLength }: IProps) => {
         style={{
           flex: 1,
           justifyContent: "center",
+          padding: 2,
         }}
       >
         {key === "backIcon" ? (
@@ -94,7 +112,10 @@ const VirtualKeyboard = ({ value, setValue, maxLength }: IProps) => {
       {makeRow(["1", "2", "3"])}
       {makeRow(["4", "5", "6"])}
       {makeRow(["7", "8", "9"])}
-      {makeRow([",", "0", "backIcon"])}
+
+      {withComma
+        ? makeRow([",", "0", "backIcon"])
+        : makeRow(["", "0", "backIcon"])}
     </View>
   );
 };
