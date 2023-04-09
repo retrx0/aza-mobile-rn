a48852431094c1f13b83877bb70011b6374e1bac
retrx0 <61639630+retrx0@users.noreply.github.com>
2023-04-10 01:38:06 +0200

    welcome screen fix

--- src/screens/onboarding/OnboardingUtil.ts
+++ src/screens/onboarding/OnboardingUtil.ts
@@ -24,28 +24,28 @@ export const carousel_data: CarouselItem[] = [
   },
   {
     id: 2,
-    source: HOME_ONBOARDING_5,
+    source: HOME_ONBOARDING_2,
     heading: `Lightning fast transactions`,
     description:
       "Make payments 24/7 at the speed of light with a 100% transaction certainty",
   },
   {
     id: 3,
-    source: HOME_ONBOARDING_2,
+    source: HOME_ONBOARDING_3,
     heading: "Bills & Payments",
     description:
       "Settle all your bills effortlessly without stepping out of your house",
   },
   {
     id: 4,
-    source: HOME_ONBOARDING_3,
+    source: HOME_ONBOARDING_4,
     heading: "Save your funds",
     description:
-      "Save and lock up your funds for a specified time frame for future use",
+      "Save and lock up your funds for a specified time frame for future use (coming soon!)",
   },
   {
     id: 5,
-    source: HOME_ONBOARDING_4,
+    source: HOME_ONBOARDING_5,
     heading: "Cutting Edge Analysis",
     description: "Keep track of your expenditure with our expert analysis",
   },
